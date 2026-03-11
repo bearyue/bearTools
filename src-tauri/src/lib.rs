002c747ec686f98b8151fa895733b2718fe69ebd
xiongyue <xiongyue@jiaxincloud.com>
2026-03-11 11:56:39 +0800

    feat: settings, autostart, and theme

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,6 @@
 use serde::Serialize;
 use std::{io::ErrorKind, path::Path, process::Command};
+use tauri_plugin_autostart::MacosLauncher;
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -233,6 +234,7 @@ fn adb_list_devices() -> Result<AdbDevicesResult, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, None))
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
